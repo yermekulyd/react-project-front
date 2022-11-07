f6e19aa4c08d0fab9f9e036d1f642de55569fdb4
Didar <yermekulyd@gmail.com>
2022-11-07 16:46:06 +0600

    giving permission to delete articles to the owner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,8 @@ import { fetchPosts, fetchTags } from '../redux/slices/posts';
 
 export const Home = () => {
   const dispatch = useDispatch();
-  const { posts, tags } = useSelector(state => state.posts);
+  const userData = useSelector((state) => state.auth.data);
+  const { posts, tags } = useSelector((state) => state.posts);
 
   const isPostsLoading = posts.status === 'loading';
   const isTagsLoading = tags.status === 'loading';
@@ -46,8 +47,7 @@ export const Home = () => {
                 viewsCount={obj.viewsCount}
                 commentsCount={3}
                 tags={obj.tags}
-                
-                isEditable
+                isEditable={userData?._id === obj.user._id}
               />
             ),
           )}
