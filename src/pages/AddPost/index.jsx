edf27ba2276dd72754e03fd4f14510ea4f01ce46
Didar <yermekulyd@gmail.com>
2022-11-07 17:18:19 +0600

    form of creation articles, react logic

--- src/pages/AddPost/index.jsx
+++ src/pages/AddPost/index.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Navigate } from "react-router-dom";
 import TextField from '@mui/material/TextField';
 import Paper from '@mui/material/Paper';
 import Button from '@mui/material/Button';
 import SimpleMDE from 'react-simplemde-editor';
 
 import 'easymde/dist/easymde.min.css';
+import { selectIsAuth } from "../../redux/slices/auth";
 import styles from './AddPost.module.scss';
 
 export const AddPost = () => {
   const imageUrl = '';
+  const isAuth = useSelector(selectIsAuth);
   const [value, setValue] = React.useState('');
+  const [title, setTitle] = React.useState('');
+  const [tags, setTags] = React.useState('');
 
-  const handleChangeFile = () => {};
+  const handleChangeFile = () => { };
 
-  const onClickRemoveImage = () => {};
+  const onClickRemoveImage = () => { };
 
   const onChange = React.useCallback((value) => {
     setValue(value);
@@ -33,6 +39,12 @@ export const AddPost = () => {
     }),
     [],
   );
+
+  if (!window.localStorage.getItem('token') && !isAuth) {
+    return <Navigate to="/" />;
+  }
+
+  //console.log({title, tags, value});
 
   return (
     <Paper style={{ padding: 30 }}>
@@ -54,9 +66,17 @@ export const AddPost = () => {
         classes={{ root: styles.title }}
         variant="standard"
         placeholder="Заголовок статьи..."
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
         fullWidth
       />
-      <TextField classes={{ root: styles.tags }} variant="standard" placeholder="Тэги" fullWidth />
+      <TextField
+        value={tags}
+        onChange={(e) => setTags(e.target.value)}
+        classes={{ root: styles.tags }} 
+        variant="standard" 
+        placeholder="Тэги" 
+        fullWidth />
       <SimpleMDE className={styles.editor} value={value} onChange={onChange} options={options} />
       <div className={styles.buttons}>
         <Button size="large" variant="contained">
